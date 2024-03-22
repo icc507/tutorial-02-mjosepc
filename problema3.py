d80da3423729a1532bbe5aa918ebc5c5c4545599
mjosepc <142420543+mjosepc@users.noreply.github.com>
2024-03-22 10:25:13 -0300

    Desarrollo problema 3

--- problema3.py
+++ problema3.py
@@ -9,5 +9,23 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def arbolBinario(numero):
+    return [numero, [], [], []]
+
+def insertaEnArbolBinario(arbol, numero):
+    if arbol == []:
+        arbol += arbolBinario(numero)
+    elif arbol[0] == numero:
+        insertaEnArbolBinario(arbol[2], numero)
+    elif numero < arbol[0]:
+        insertaEnArbolBinario(arbol[1], numero)
+    else:
+        insertaEnArbolBinario(arbol[3], numero)
+    return arbol 
+
+numeros = input().split()
+arbol = []
+for numero in numeros:
+    arbol = insertaEnArbolBinario(arbol, int(numero)) 
+
+print(arbol)
