d7781e32ded9b3ab6960bbb15e536c91a0d01836
mjosepc <142420543+mjosepc@users.noreply.github.com>
2024-03-22 09:14:06 -0300

    Desarrollo problema 2

--- problema2.py
+++ problema2.py
@@ -7,5 +7,6 @@
 #         20 90 hola jiji 77
 #La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
-t = input()
-print(t)
+t = input().split()
+tupla1 = tuple(int(x) if x.isdigit() else x for x in t)
+print(tupla1[::-1])
